87995ce1683f0037154f64848628d7e6abeff051
jafinley <117559119+jafinley@users.noreply.github.com>
2023-09-05 15:19:48 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,27 +1,93 @@
-// Authorization token that must have been created previously. See : https://developer.spotify.com/documentation/web-api/concepts/authorization
-const token = 'BQAeF4KKmjjZrUmzCfmeRMKTGShBsJSGOkb8ySa73wU51Y3lqAhPhcMXJWW8jYvTAwMMPvlQV8VOCe2XYLwAzbKsl20J14rxjMdEOIpbGOWG49GjsOIy4FyPFbdzPccxo5fZzmgg7mllkzRrVQsIpNNm9Bn0BHbAQNgpJSnCPoAwd4o6K3gsSD6v0zcvmysHJSi9_GUUc7B6tkit48Lnah5bNGwP-WUywNcRlPC1s6KBW-tOdUnaEaeP2s2nCpL7ayjjdAFRvLE';
-async function fetchWebApi(endpoint, method, body) {
-  const res = await fetch(`https://api.spotify.com/${endpoint}`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-    method,
-    body:JSON.stringify(body)
+function generateRandomString(length) {
+    let text = '';
+    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  
+    for (let i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
+codeVerifier = generateRandomString(128);
+const digest = await window.crypto.subtle.digest('SHA-256', data);
+async function generateCodeChallenge(codeVerifier) {
+    function base64encode(string) {
+      return btoa(String.fromCharCode.apply(null, new Uint8Array(string)))
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_')
+        .replace(/=+$/, '');
+    }
+  
+    const encoder = new TextEncoder();
+    const data = encoder.encode(codeVerifier);
+    const digest = await window.crypto.subtle.digest('SHA-256', data);
+  
+    return base64encode(digest);
+}
+  
+const clientId = 'e1b29c8020f24572a1d44274d477e585';
+const redirectUri = 'http://localhost:8080';
+
+generateCodeChallenge(codeVerifier).then(codeChallenge => {
+  let state = generateRandomString(16);
+  let scope = 'user-read-private user-read-email';
+
+  localStorage.setItem('code_verifier', codeVerifier);
+
+  let args = new URLSearchParams({
+    response_type: 'code',
+    client_id: clientId,
+    scope: scope,
+    redirect_uri: redirectUri,
+    state: state,
+    code_challenge_method: 'S256',
+    code_challenge: codeChallenge
   });
-  return await res.json();
-}
 
-async function getTopTracks(){
-  // Endpoint reference : https://developer.spotify.com/documentation/web-api/reference/get-users-top-artists-and-tracks
-  return (await fetchWebApi(
-    'v1/me/top/tracks?time_range=short_term&limit=5', 'GET'
-  )).items;
-}
+  window.location = 'https://accounts.spotify.com/authorize?' + args;
+});
 
-const topTracks = await getTopTracks();
-console.log(
-  topTracks?.map(
-    ({name, artists}) =>
-      `${name} by ${artists.map(artist => artist.name).join(', ')}`
-  )
-);
+const urlParams = new URLSearchParams(window.location.search);
+let code = urlParams.get('code');
+
+let codeVerifier = localStorage.getItem('code_verifier');
+
+let body = new URLSearchParams({
+  grant_type: 'authorization_code',
+  code: code,
+  redirect_uri: redirectUri,
+  client_id: clientId,
+  code_verifier: codeVerifier
+});
+
+const response = fetch('https://accounts.spotify.com/api/token', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded'
+  },
+  body: body
+})
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('HTTP status ' + response.status);
+    }
+    return response.json();
+  })
+  .then(data => {
+    localStorage.setItem('access_token', data.access_token);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+});
+
+  async function getProfile(accessToken) {
+    let accessToken = localStorage.getItem('access_token');
+  
+    const response = await fetch('https://api.spotify.com/v1/me', {
+      headers: {
+        Authorization: 'Bearer ' + accessToken
+      }
+    });
+  
+    const data = await response.json();
+}
+  
