ce15c68a5a4997d1ebcc2a28a28cb58045f2960c
jafinley <117559119+jafinley@users.noreply.github.com>
2023-09-15 15:32:17 -0500

    Update test.js

--- test.js
+++ test.js
@@ -11,3 +11,42 @@ codeVerifier = generateRandomString(128);
 
 var contentHolder = document.getElementById('auth');
 contentHolder.innerHTML = codeVerifier; 
+
+const digest = await window.crypto.subtle.digest('SHA-256', data);
+console.log(digest);
+async function generateCodeChallenge(codeVerifier) {
+    function base64encode(string) {
+      return btoa(String.fromCharCode.apply(null, new Uint8Array(string)))
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_')
+        .replace(/=+$/, '');
+    }
+  
+    const encoder = new TextEncoder();
+    const data = encoder.encode(codeVerifier);
+    const digest = await window.crypto.subtle.digest('SHA-256', data);
+  
+    return base64encode(digest);
+}
+
+const clientId = 'e1b29c8020f24572a1d44274d477e585';
+const redirectUri = 'http://localhost:8080';
+
+generateCodeChallenge(codeVerifier).then(codeChallenge => {
+  let state = generateRandomString(16);
+  let scope = 'user-read-private user-read-email';
+
+  localStorage.setItem('code_verifier', codeVerifier);
+
+  let args = new URLSearchParams({
+    response_type: 'code',
+    client_id: clientId,
+    scope: scope,
+    redirect_uri: redirectUri,
+    state: state,
+    code_challenge_method: 'S256',
+    code_challenge: codeChallenge
+  });
+
+  window.location = 'https://accounts.spotify.com/authorize?' + args;
+});
