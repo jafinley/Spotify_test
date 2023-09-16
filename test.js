e3f950ea759ea6aadca94f94f17e428300e634c7
jafinley <117559119+jafinley@users.noreply.github.com>
2023-09-16 16:15:02 -0500

    Update test.js

--- test.js
+++ test.js
@@ -61,6 +61,18 @@ let body = new URLSearchParams({
   code_verifier: codeVerifier
 });
 
+async function getProfile(accessToken) {
+  let accessToken = localStorage.getItem('access_token');
+
+  const response = await fetch('https://api.spotify.com/v1/me', {
+    headers: {
+      Authorization: 'Bearer ' + accessToken
+    }
+  });
+
+  const data = await response.json();
+}
+
 const response = fetch('https://accounts.spotify.com/api/token', {
   method: 'POST',
   headers: {
@@ -80,15 +92,3 @@ const response = fetch('https://accounts.spotify.com/api/token', {
   .catch(error => {
     console.error('Error:', error);
   });
-
-async function getProfile(accessToken) {
-  let accessToken = localStorage.getItem('access_token');
-
-  const response = await fetch('https://api.spotify.com/v1/me', {
-    headers: {
-      Authorization: 'Bearer ' + accessToken
-    }
-  });
-
-  const data = await response.json();
-}
